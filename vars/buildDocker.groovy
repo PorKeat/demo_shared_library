8556dd7d1a6bfc7dd5c86ff938a48e36690b00d2
Porkeat <alexkgm2412@gmail.com>
2026-01-13 18:39:41 +0700

    change docker-file spring

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,32 +1,11 @@
+// vars/deploySpring.groovy
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('Prepare Docker Files') {
-                steps {
-                    script {
-                        // Load Dockerfile and docker-compose.yml from shared library
-                        writeFile file: 'Dockerfile', text: libraryResource('spring/dev.Dockerfile')
-                        writeFile file: 'docker-compose.yml', text: libraryResource('spring/docker-compose.yml')
-                    }
-                }
-            }
+    script {
+        // Load Dockerfile and docker-compose.yml from shared library
+        writeFile file: 'Dockerfile', text: libraryResource('spring/dev.Dockerfile')
+        writeFile file: 'docker-compose.yml', text: libraryResource('spring/docker-compose.yml')
 
-
-            stage('Build and Run Docker Compose') {
-                steps {
-                    script {
-                        sh "docker compose up -d --build"
-                    }
-                }
-            }
-        }
-
-        post {
-            always {
-                echo "Cleaning up..."
-                sh "docker-compose down"
-            }
-        }
+        // Build and run Docker Compose
+        sh "docker compose up -d --build"
     }
 }
