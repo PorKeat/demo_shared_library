ea3aeb77951234c439fa560d883018c837a9780e
Porkeat <alexkgm2412@gmail.com>
2026-01-13 18:41:06 +0700

    change docker-file spring

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -2,7 +2,7 @@
 def call() {
     script {
         // Load Dockerfile and docker-compose.yml from shared library
-        writeFile file: 'Dockerfile', text: libraryResource('spring/dev.Dockerfile')
+        writeFile file: 'dev.Dockerfile', text: libraryResource('spring/dev.Dockerfile')
         writeFile file: 'docker-compose.yml', text: libraryResource('spring/docker-compose.yml')
 
         // Build and run Docker Compose
